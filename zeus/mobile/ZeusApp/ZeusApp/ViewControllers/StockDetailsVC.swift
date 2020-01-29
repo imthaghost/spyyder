231cc70ce7b1c641b5395d2d791685b5c255f386
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-29 15:33:35 -0800

    [add] setup view controller

--- zeus/mobile/ZeusApp/ZeusApp/ViewControllers/StockDetailsVC.swift
+++ zeus/mobile/ZeusApp/ZeusApp/ViewControllers/StockDetailsVC.swift
@@ -9,22 +9,27 @@
 import UIKit
 
 class StockDetailsVC: UIViewController {
-
+//MARK: Properties
+    
+//MARK: IBOutlets
+    
+//MARK: App Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//MARK: Private Methods
+    fileprivate func setupViews() {
+        view.backgroundColor = .red
     }
-    */
-
+    
+//MARK: IBActions
+    
+//MARK: Helpers
+    
 }
+
+//MARK: Extensions
+
+
