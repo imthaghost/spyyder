cf93cf3f368e2ebd536edfa7601e045a939cd425
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-28 19:44:42 -0800

    [add] init the xcode project

---
+++ iOSApp/ZeusApp/ZeusApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZeusApp
+//
+//  Created by Macbook Pro 15 on 1/28/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
