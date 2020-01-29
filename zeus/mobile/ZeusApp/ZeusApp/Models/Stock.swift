8363a01ec8543f946f475d599c8a35aed5388212
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-29 15:32:04 -0800

    [add] view controllers

---
+++ zeus/mobile/ZeusApp/ZeusApp/Models/Stock.swift
@@ -0,0 +1,9 @@
+//
+//  Stock.swift
+//  ZeusApp
+//
+//  Created by Macbook Pro 15 on 1/29/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import Foundation
---
+++ zeus/mobile/ZeusApp/ZeusApp/ViewControllers/StockDetailsVC.swift
@@ -0,0 +1,30 @@
+//
+//  StockDetailsVC.swift
+//  ZeusApp
+//
+//  Created by Macbook Pro 15 on 1/29/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class StockDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
