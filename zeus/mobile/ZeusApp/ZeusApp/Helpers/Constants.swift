2741e847acbd80f5486e9c1a0c0bf139bfdd6e8c
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-30 18:41:32 -0800

    [add] constants for the images

---
+++ zeus/mobile/ZeusApp/ZeusApp/Helpers/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  ZeusApp
+//
+//  Created by Macbook Pro 15 on 1/30/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+let kDEFAULTIMAGE: UIImage = UIImage(named: "blankImage")!
+let kMENUIMAGE: UIImage = UIImage(named: "menu")!
