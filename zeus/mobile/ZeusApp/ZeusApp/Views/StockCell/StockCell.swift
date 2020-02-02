511ad47a12e0b2f3fa86b2a9c16a8db3de79746e
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-01 16:40:22 -0800

    [add] init the tableviewCell

--- zeus/mobile/ZeusApp/ZeusApp/Views/StockCell/StockCell.swift
+++ zeus/mobile/ZeusApp/ZeusApp/Views/StockCell/StockCell.swift
@@ -2,19 +2,14 @@
 //  StockCell.swift
 //  ZeusApp
 //
-//  Created by Macbook Pro 15 on 1/30/20.
+//  Created by Macbook Pro 15 on 2/1/20.
 //  Copyright © 2020 SamuelFolledo. All rights reserved.
 //
 
 import UIKit
 
 class StockCell: UITableViewCell {
-    
-    var stock: Stock!
-    
-    @IBOutlet weak var stockImageView: UIImageView!
-    @IBOutlet weak var nameLabel: UILabel!
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -22,12 +17,8 @@ class StockCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+
         // Configure the view for the selected state
     }
     
-    func populateViews() {
-//        stockImageView.downloaded(fromLink: mail.imageUrl)
-        stockImageView.image = kDEFAULTIMAGE
-        nameLabel.text = stock.name
-    }
 }
