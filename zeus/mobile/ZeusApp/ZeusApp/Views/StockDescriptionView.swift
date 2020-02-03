0475da0851b5c94593ff48a7cfcae22d2b3f21f5
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-03 12:15:52 -0800

    [add] init stockDescriptionView

---
+++ zeus/mobile/ZeusApp/ZeusApp/Views/StockDescriptionView.swift
@@ -0,0 +1,21 @@
+//
+//  StockDescriptionView.swift
+//  ZeusApp
+//
+//  Created by Macbook Pro 15 on 2/3/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class StockDescriptionView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
