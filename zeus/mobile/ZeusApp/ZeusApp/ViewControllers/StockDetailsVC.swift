84bda1cecc807ddd9f43d314331d5d998b419f6c
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-03 12:16:55 -0800

    [add] init the stockDescriptionView and added a flip effect on stockView tap

--- zeus/mobile/ZeusApp/ZeusApp/ViewControllers/StockDetailsVC.swift
+++ zeus/mobile/ZeusApp/ZeusApp/ViewControllers/StockDetailsVC.swift
@@ -11,6 +11,21 @@ import UIKit
 class StockDetailsVC: UIViewController {
 //MARK: Properties
     var stock: Stock!
+    let graphView: UIView = {
+        let view: UIView = UIView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .black
+        return view
+    }()
+    var descriptionView: StockDescriptionView!
+    var isDescriptionView: Bool = false
+//    {
+////        let view: StockDescriptionView = UIView(frame: .zero)
+//        let view = StockDescriptionView(stock: stock)
+//        view.translatesAutoresizingMaskIntoConstraints = false
+//        view.backgroundColor = kMAINCOLOR
+//        return view
+//    }()
     
 //MARK: IBOutlets
     @IBOutlet weak var stockView: UIView!
@@ -26,6 +41,23 @@ class StockDetailsVC: UIViewController {
     fileprivate func setupViews() {
         navItem.title = stock.name
         stockView.backgroundColor = kMAINCOLOR
+        stockView.isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(flipStockViewTap(_:)))
+        stockView.addGestureRecognizer(tap)
+        
+        descriptionView = StockDescriptionView(stock: stock)
+        descriptionView.translatesAutoresizingMaskIntoConstraints = false
+        stockView.addSubview(descriptionView)
+        NSLayoutConstraint.activate(
+            descriptionView.pinToEdges(view: self.stockView) //pin to edges of stockView
+        )
+        
+        stockView.addSubview(graphView)
+        NSLayoutConstraint.activate(
+            graphView.pinToEdges(view: self.stockView)
+        )
+        descriptionView.isHidden = false
+        
     }
     
 //MARK: IBActions
@@ -34,7 +66,19 @@ class StockDetailsVC: UIViewController {
     }
     
 //MARK: Helpers
-    
+    @objc func flipStockViewTap(_ gesture: UITapGestureRecognizer) { //dismiss fields
+        //        stockView.endEditing(false)
+        isDescriptionView = !isDescriptionView
+        let transitionOptions: UIView.AnimationOptions = [.transitionFlipFromRight, .showHideTransitionViews]
+        
+        UIView.transition(with: graphView, duration: 0.5, options: transitionOptions, animations: {
+            self.graphView.isHidden = self.isDescriptionView
+        })
+        
+        UIView.transition(with: descriptionView, duration: 0.5, options: transitionOptions, animations: {
+            self.descriptionView.isHidden = !self.isDescriptionView
+        })
+    }
 }
 
 //MARK: Extensions
