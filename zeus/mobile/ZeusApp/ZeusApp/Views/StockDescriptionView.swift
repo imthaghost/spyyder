732b746681c8df45baedbac8690e8012b70ff766
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-03 12:16:32 -0800

    [add] StockDescription's UI constraints and populated the views

--- zeus/mobile/ZeusApp/ZeusApp/Views/StockDescriptionView.swift
+++ zeus/mobile/ZeusApp/ZeusApp/Views/StockDescriptionView.swift
@@ -9,13 +9,46 @@
 import UIKit
 
 class StockDescriptionView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var ceoLabel: UILabel!
+    @IBOutlet weak var hqLabel: UILabel!
+    @IBOutlet weak var foundedLabel: UILabel!
+    @IBOutlet weak var sharesLabel: UILabel!
+    
+    var stock: Stock!
+    
+    required init(stock: Stock) {
+        super.init(frame: .zero)
+        self.stock = stock
+//        self.ceoLabel.text = stock.shortName
+        commonInit()
     }
-    */
-
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    fileprivate func commonInit() {
+        Bundle.main.loadNibNamed("StockDescriptionView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        setupBackgroundLayer()
+        
+    }
+    
+    fileprivate func setupBackgroundLayer() {
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.bounds
+        gradientLayer.colors = [kMAINCOLOR.cgColor, UIColor.black.cgColor] //add colors, first one will be at the top
+        gradientLayer.locations = [0.5, 1]
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
 }
