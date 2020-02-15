3c34977470c551ff83e1bd4b236d76122478d8a1
Tyler_Holland <tyler.holland@students.makeschool.com>
2020-02-14 21:51:57 -0800

    [update]

--- webapp/application/controllers/email.py
+++ webapp/application/controllers/email.py
@@ -6,10 +6,9 @@ email_route = Blueprint('email', __name__, static_folder='static')
 
 @email_route.route('/email', methods=['GET', 'POST'])
 def email():
-    
-    email = request.form.get('email')
 
     if request.method == 'POST':
+        email = request.form.get('email')
         return jsonify({'email': email})
     else:
         return render_template('index.html')
