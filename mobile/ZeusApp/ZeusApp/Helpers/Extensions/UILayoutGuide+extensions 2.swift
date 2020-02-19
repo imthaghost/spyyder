3fd5abd428302d79857e82eabe2ee9969415e26f
Andre Williams <awjrs22@gmail.com>
2020-02-19 12:34:46 -0800

    update

---
+++ mobile/ZeusApp/ZeusApp/Helpers/Extensions/UILayoutGuide+extensions 2.swift
@@ -0,0 +1,21 @@
+//
+//  UILayoutGuide+extensions.swift
+//  ZeusApp
+//
+//  Created by Macbook Pro 15 on 2/3/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func pinToEdges(view: UIView, inset: UIEdgeInsets = UIEdgeInsets.zero) -> [NSLayoutConstraint] { //pins view to edge - returns array of constraints that needs to be activated
+        return [
+            leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: inset.left),
+            trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: inset.right),
+            topAnchor.constraint(equalTo: view.topAnchor, constant: inset.top),
+            bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: inset.bottom)
+        ]
+    }
+}
+
---
+++ mobile/ZeusApp/ZeusApp/ViewControllers/StockDetailsVC 2.swift
@@ -0,0 +1,75 @@
+//
+//  StockDetailsVC.swift
+//  ZeusApp
+//
+//  Created by Macbook Pro 15 on 1/29/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class StockDetailsVC: UIViewController {
+//MARK: Properties
+    var stock: Stock!
+    let graphView: UIView = {
+        let view: UIView = UIView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .black
+        return view
+    }()
+    var descriptionView: StockDescriptionView!
+    var isDescriptionView: Bool = false
+    
+//MARK: IBOutlets
+    @IBOutlet weak var stockView: UIView!
+    
+//MARK: App Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+//MARK: Private Methods
+    fileprivate func setupViews() {
+        self.title = stock.name
+        stockView.backgroundColor = kMAINCOLOR
+        stockView.isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(flipStockViewTap(_:)))
+        stockView.addGestureRecognizer(tap)
+        
+        descriptionView = StockDescriptionView(stock: stock)
+        stockView.addSubview(descriptionView)
+        NSLayoutConstraint.activate(
+            descriptionView.pinToEdges(view: self.stockView) //pin to edges of stockView
+        )
+        
+        stockView.addSubview(graphView)
+        NSLayoutConstraint.activate(
+            graphView.pinToEdges(view: self.stockView)
+        )
+        descriptionView.isHidden = false
+        
+    }
+    
+//MARK: IBActions
+    @IBAction func backButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+//MARK: Helpers
+    @objc func flipStockViewTap(_ gesture: UITapGestureRecognizer) { //dismiss fields
+        //        stockView.endEditing(false)
+        isDescriptionView = !isDescriptionView
+        let transitionOptions: UIView.AnimationOptions = [.transitionFlipFromRight, .showHideTransitionViews]
+        
+        UIView.transition(with: graphView, duration: 0.5, options: transitionOptions, animations: {
+            self.graphView.isHidden = self.isDescriptionView
+        })
+        
+        UIView.transition(with: descriptionView, duration: 0.5, options: transitionOptions, animations: {
+            self.descriptionView.isHidden = !self.isDescriptionView
+        })
+    }
+}
+
+//MARK: Extensions
