b95f8a34d804a56bf5439317e655a8b543d3f413
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-21 22:40:26 -0800

    [add] tabbar item animation on tap

--- mobile/ZeusApp/ZeusApp/Controllers/TabBarController/MainTabBarController.swift
+++ mobile/ZeusApp/ZeusApp/Controllers/TabBarController/MainTabBarController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class MainTabBarController: UITabBarController {
-
+    ///Bounce animation for clicking on tabbar items
+    private var bounceAnimation: CAKeyframeAnimation = {
+        let bounceAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
+        bounceAnimation.values = [1.0, 1.4, 0.9, 1.02, 1.0]
+        bounceAnimation.duration = TimeInterval(0.3)
+        bounceAnimation.calculationMode = CAAnimationCalculationMode.cubic
+        return bounceAnimation
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTabBar()
@@ -53,7 +61,18 @@ extension MainTabBarController: UITabBarControllerDelegate {
 //        return false
 //    }
     
+    ///Adds a sliding animation whenever switching between VC on the tab
     func tabBarController(_ tabBarController: UITabBarController, animationControllerForTransitionFrom fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         return TransitioningObject()
+    }
+    
+    /// didSelect that can trigger animation
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        // find index if the selected tab bar item, then find the corresponding view and get its image, the view position is offset by 1 because the first item is the background (at least in this case)
+        guard let idx = tabBar.items?.firstIndex(of: item), tabBar.subviews.count > idx + 1, let imageView = tabBar.subviews[idx + 1].subviews.compactMap ({ $0 as? UIImageView }).first else {
+            return
+        }
+        // animate the imageView
+        imageView.layer.add(bounceAnimation, forKey: nil)
     }
 }
