32699cfa683fa83a3210c11edb18b274683e80e6
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-23 20:01:38 -0800

    [add] networking login, create, and get user methods

--- mobile/ZeusApp/ZeusApp/Auth/Models/Networking.swift
+++ mobile/ZeusApp/ZeusApp/Auth/Models/Networking.swift
@@ -21,4 +21,132 @@ struct Message: Decodable {
     let success: String
 }
 
+//MARK: Network Calls
+func postRequest(url: String, dic: [String: Any], completion: @escaping(_ error: String?, _ user: User?) -> Void) { //user for Registering user
+    if (!JSONSerialization.isValidJSONObject(dic)) {
+        completion("Invalid email and password data", nil)
+    }
+    let session = URLSession.shared
+    let request = NSMutableURLRequest(url: NSURL(string: url)! as URL)
+    request.httpMethod = "POST"
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.addValue("application/json", forHTTPHeaderField: "Accept")
+    do {
+        request.httpBody = try JSONSerialization.data(withJSONObject: dic, options: .prettyPrinted)
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {(data, response, error) in
+            if let response = response {
+                let nsHTTPResponse = response as! HTTPURLResponse
+                let statusCode = nsHTTPResponse.statusCode
+                print ("status code = \(statusCode)")
+            }
+            if let error = error {
+                print ("\(error)")
+            }
+            if let data = data {
+//                do {
+//                    let response = try JSONDecoder().decode(Token.self, from: data)
+//                    print(response.token)
+//                } catch { print(error) }
+                do { //MARK: On /create, error is JSON not formatted. On /user error is dataCorrupted... Invalid value around character
+                    let jsonResponse = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions())
+                    print ("jsonResponse: \(jsonResponse)")
+                    guard let response = jsonResponse as? [String: Any] else { completion("Error converting json received", nil); return }
+                    if let errorMessage = response[kMESSAGE] as? String { //if we get a message response, it means there is an error
+                        print("Error = \(errorMessage)")
+                        completion(errorMessage, nil)
+                    }
+                    if let token = response[kTOKEN] {
+                        print("Token = ", response[kTOKEN] ?? "No token")
+                    }
+                    //Should not reach here, display unknown error
+                    completion("Unknown response error", nil)
+                } catch _ {
+                    completion("JSON not formatted", nil)
+                }
+            }
+        })
+        task.resume()
+    } catch _ {
+        print ("Some error :)")
+    }
+}
 
+func login(email: String, password: String, completion: @escaping(_ error: String?, _ user: User?) -> Void) {
+    var userDic:[String: String] = [
+        kEMAIL: email,
+        "password": password,
+    ]
+    if (!JSONSerialization.isValidJSONObject(userDic)) {
+        completion("Invalid email and password data", nil)
+    }
+    let session = URLSession.shared
+    let url = "http://3.17.150.127:8000/login"
+    let request = NSMutableURLRequest(url: NSURL(string: url)! as URL)
+    request.httpMethod = "POST"
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    do {
+        request.httpBody = try JSONSerialization.data(withJSONObject: userDic, options: .prettyPrinted)
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {(data, response, error) in
+            if let response = response {
+                let nsHTTPResponse = response as! HTTPURLResponse
+                let statusCode = nsHTTPResponse.statusCode
+                print ("status code = \(statusCode)")
+            }
+            if let error = error {
+                completion(error.localizedDescription, nil)
+            }
+            if let data = data {
+                do {
+                    let response = try JSONDecoder().decode(Token.self, from: data) //MARK: Handle Error here in case request does not return any Token
+                    userDic[kTOKEN] = response.token
+                    getRequestUser(userDic: userDic) { (error, user) in
+                        if let error = error {
+                            completion(error, nil)
+                        }
+                        completion(nil, user!)
+                    }
+                } catch {
+                    completion(error.localizedDescription, nil)
+                }
+//                do {
+//                    let jsonResponse = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions())
+//                    print ("jsonResponse: \(jsonResponse)")
+//                } catch _ { print ("JSON not formatted") }
+            }
+        })
+        task.resume()
+    } catch _ {
+        completion("Login request error", nil)
+    }
+}
+
+///Takes a token and performs a GET request from the API and returns a user from the database if no error
+func getRequestUser(userDic: [String: Any], completion: @escaping(_ error: String?, _ user: User?) -> Void) {
+    guard let token = userDic[kTOKEN] as? String else { completion("No token", nil); return }
+    let session = URLSession.shared
+    let url = "\(kAPIURL)/user"
+    let request = NSMutableURLRequest(url: NSURL(string: url)! as URL)
+    request.httpMethod = "GET"
+    request.setValue(token, forHTTPHeaderField: "token") //set token to the header
+    let task = session.dataTask(with: request as URLRequest, completionHandler: {(data, response, error) in
+        if let response = response {
+            let nsHTTPResponse = response as! HTTPURLResponse
+            let statusCode = nsHTTPResponse.statusCode
+            print ("status code = \(statusCode)")
+        }
+        if let error = error {
+            completion(error.localizedDescription, nil)
+        }
+        if let data = data {
+            do {
+                let response = try JSONDecoder().decode(Message.self, from: data)
+                print(response.success)
+                let user = User(_dictionary: userDic)
+                completion(nil, user)
+            } catch {
+                completion(error.localizedDescription, nil)
+            }
+        }
+    })
+    task.resume()
+}
