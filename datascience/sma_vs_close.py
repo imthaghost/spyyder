9258fe3a12780fb2cf3986f3deaf1c7ae2c4d903
Andre Williams <awjrs22@gmail.com>
2020-02-24 19:36:55 -0800

    gets rid of unnecessary code

--- datascience/sma_vs_close.py
+++ datascience/sma_vs_close.py
@@ -9,16 +9,16 @@ from alpha_vantage.techindicators import TechIndicators # calculate sma for us
 import matplotlib.pyplot as plt 
 
 
-api_key = 'IOLIXAXKGPI3A4QM'
+# api_key = 'IOLIXAXKGPI3A4QM'
 
-ts = TimeSeries(key=api_key, output_format='pandas')
+# ts = TimeSeries(key=api_key, output_format='pandas')
 
-data_ts, meta_data_ts = ts.get_intraday(symbol='MSFT', interval='1min', outputsize='full') # all data, every minute, from MSFT
+# data_ts, meta_data_ts = ts.get_intraday(symbol='MSFT', interval='1min', outputsize='full') # all data, every minute, from MSFT
 
-period = 60 # 1 hour for sma
+# period = 60 # 1 hour for sma
 
-ti = TechIndicators(key=api_key, output_format='pandas')
-data_ti, meta_data_ti = ti.get_sma(symbol='MSFT', interval='1min', time_period=period, series_type='close')
+# ti = TechIndicators(key=api_key, output_format='pandas')
+# data_ti, meta_data_ti = ti.get_sma(symbol='MSFT', interval='1min', time_period=period, series_type='close')
 
 def get(data):
     data = str(data) 
@@ -41,7 +41,8 @@ def api(new_data):
     ti = TechIndicators(key=api_key, output_format='pandas')
     data_ti, meta_data_ti = ti.get_sma(symbol='MSFT', interval='1min', time_period=period, series_type='close')
     df2 = data_ts['4. close'].iloc[-1] # we only want the closing column
-    return ('this is data: ' +  str(new_data) + str( df2)) # prints last closing price of chosen company
+    return {'company': new_data,'price': df2}
+   # return ('this is data: ' +  str(new_data) + str( df2)) # prints last closing price of chosen company
 
 print(get('microsoft'))
 
