be18fff3c676531d52658208458aad08f73c8d59
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-24 18:48:13 -0800

    [add] load save user stocks

--- mobile/ZeusApp/ZeusApp/Controllers/MyStocksVC.swift
+++ mobile/ZeusApp/ZeusApp/Controllers/MyStocksVC.swift
@@ -23,11 +23,7 @@ class MyStocksVC: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        if let user = getCurrentUser() {
-            user.setNeedsToReloadStocks()
-            self.stocks = user.stocks
-            tableView.reloadData()
-        }
+        populateStocks()
     }
     
 //MARK: Navigation
@@ -43,15 +39,14 @@ class MyStocksVC: UIViewController {
     }
     
 //MARK: Private Methods
-    fileprivate func setupViews() {
-        self.title = "My Stocks"
-        self.navigationController!.navigationBar.isTranslucent = false
-//        createTestStocks()
-        setupTableView()
+    fileprivate func populateStocks() {
         if let user = getCurrentUser() {
             user.setNeedsToReloadStocks()
-            self.stocks = user.stocks
-            tableView.reloadData()
+            let userStocks = user.getUserStocks()
+            DispatchQueue.main.async {
+                self.stocks = userStocks
+                self.tableView.reloadData()
+            }
         } else { //if no user...
             deleteCurrentUser()
             let vc = UIStoryboard(name: "Auth", bundle: nil).instantiateInitialViewController()!
@@ -59,27 +54,19 @@ class MyStocksVC: UIViewController {
         }
     }
     
+    fileprivate func setupViews() {
+        self.title = "My Stocks"
+        self.navigationController!.navigationBar.isTranslucent = false
+
+        setupTableView()
+        populateStocks()
+    }
+    
     fileprivate func setupTableView() {
         tableView.register(UINib(nibName: "StockCell", bundle: nil), forCellReuseIdentifier: "stockCell")
         tableView.delegate = self
         tableView.dataSource = self
         tableView.tableFooterView = UIView() //removes extra unpopulated cells
-    }
-    
-    fileprivate func createTestStocks() {
-        let stock1 = Stock(_name: "Bitcoin", _shortName: "BTC", _price: "8,900", _imageUrl: "")
-        let stock2 = Stock(_name: "Etherium", _shortName: "ETH", _price: "80", _imageUrl: "")
-        let stock3 = Stock(_name: "Tesla", _shortName: "TSL", _price: "600", _imageUrl: "")
-        let stock4 = Stock(_name: "Apple", _shortName: "APL", _price: "8,900", _imageUrl: "")
-        let stock5 = Stock(_name: "Bitcoin", _shortName: "BTC", _price: "8,900", _imageUrl: "")
-        let stock6 = Stock(_name: "Etherium", _shortName: "ETH", _price: "80", _imageUrl: "")
-        let stock7 = Stock(_name: "Tesla", _shortName: "TSL", _price: "600", _imageUrl: "")
-        let stock8 = Stock(_name: "Apple", _shortName: "APL", _price: "8,900", _imageUrl: "")
-        let stock9 = Stock(_name: "Bitcoin", _shortName: "BTC", _price: "8,900", _imageUrl: "")
-        let stock10 = Stock(_name: "Etherium", _shortName: "ETH", _price: "80", _imageUrl: "")
-        let stock11 = Stock(_name: "Tesla", _shortName: "TSL", _price: "600", _imageUrl: "")
-        let stock12 = Stock(_name: "Apple", _shortName: "APL", _price: "8,900", _imageUrl: "")
-        stocks.append(contentsOf: [stock1, stock2, stock3, stock4, stock5, stock6, stock7, stock8, stock9, stock10, stock11, stock12])
     }
     
 //MARK: IBActions
@@ -92,8 +79,6 @@ class MyStocksVC: UIViewController {
 extension MyStocksVC: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         return
-//        let stock = stocks[indexPath.row]
-//        self.performSegue(withIdentifier: kSEGUETOSTOCKDETAILSVC, sender: stock)
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
