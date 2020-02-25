fd170f3327945cfec245ba4bf3f117609de4c5cf
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-24 22:00:21 -0800

    [add] charts now gets displayed on stock details

--- mobile/ZeusApp/ZeusApp/Controllers/SecondaryControllers/StockDetailsVC.swift
+++ mobile/ZeusApp/ZeusApp/Controllers/SecondaryControllers/StockDetailsVC.swift
@@ -30,6 +30,11 @@ class StockDetailsVC: UIViewController {
         WhyModel(title: "Tesla increase may affect \(stock.name) prices", description: "sdjsfj sljfksjkf sjfklsjf "),
         WhyModel(title: "\(stock.name) raise $5 million on 2nd round", description: "Update your app and invest ment")
     ]
+    var dataEntries: [ChartDataEntry] = []
+        var time: [Double] = [9,10,11,12,1,2,3,4,5,6]
+    //    let timeStamps: [Int] = [9, 10, 11, 12, 1, 2, 3, 4, 5, 6]
+        let timeStamps: [String] = ["9:00", "9:05", "9:10", "9:15", "9:20", "9:25", "9:30", "9:35", "9:40", "9:45", "9:50", "9:55", "10:00",  "10:05", "10:10", "10:15", "10:20", "10:25", "10:30", "10:35", "10:40", "10:45", "10:50", "10:55", "11:00", "11:05", "11:10", "11:15", "11:20", "11:25", "11:30", "11:35", "11:40", "11:45",  "11:50", "11:55", "12:00", "12:05", "12:10", "12:15", "12:20", "12:25", "12:30", "12:35", "12:40", "12:45", "12:50", "12:55","1:00", "1:05", "1:10", "1:15", "1:20", "1:25", "1:30", "1:35", "1:40", "1:45", "1:50", "1:55", "2:00", "2:05", "2:10", "2:15", "2:20", "2:25", "2:30", "2:35", "2:40", "2:45", "2:50", "2:55", "3:00", "3:05", "3:10", "3:15", "3:20", "3:25", "3:30", "3:35", "3:40", "3:45", "3:50", "3:55", "4:00", "4:05", "4:10", "4:15", "4:20", "4:25", "4:30", "4:35", "4:40", "4:45", "4:50", "4:55", "5:00", "5:05", "5:10", "5:15", "5:20", "5:25", "5:30", "5:35", "5:40", "5:45", "5:50", "5:55", "6:00"]
+    let stockPrices: [Double] = [24.0, 39, 0.8, -10, -13, -25, -90, 50, 64, 32, 43]
     
 //MARK: IBOutlets
     @IBOutlet weak var nameLabel: UILabel!
@@ -76,6 +81,7 @@ class StockDetailsVC: UIViewController {
         setupTopLabels()
         populateWhyStackView()
         setupDescriptionView()
+        setupGraphView()
     }
     
     fileprivate func setupDescriptionView() {
@@ -160,3 +166,62 @@ class StockDetailsVC: UIViewController {
 }
 
 //MARK: Extensions
+/// Line Chart Codes
+extension StockDetailsVC {
+    fileprivate func setupGraphView() {
+        //get stocks
+        struct StockPrice {
+            var timestamp: Int
+            var price: Double
+        }
+        var prices: [StockPrice] = []
+        for i in 0..<stockPrices.count {
+            let stockPrice = StockPrice(timestamp: i+9, price: self.stockPrices[i])
+//            let stock = Stock(_name: self.stock.name, _price: String(self.stockPrices[i]))
+////            let stock = Stock(timestamp: i+9, price: stockPrices[i])
+            prices.append(stockPrice)
+        }
+        
+        graphView.xAxis.axisMaximum = Double(timeStamps.count)
+        var chartEntries: [ChartDataEntry] = []
+        var xStrings: [String] = []
+//        let sortedStockPrices = prices.sorted { (s1: Stock, s2: Stock) -> Bool in
+//            return s1.
+//        }
+        let sortedentriesData = prices.sorted { (s1: StockPrice, s2: StockPrice) -> Bool in //sort by timestamp
+            return s1.timestamp < s2.timestamp
+        }
+        for (i, price) in sortedentriesData.enumerated() {
+            let newStock = ChartDataEntry(x: Double(i), y: price.price)
+            chartEntries.append(newStock) //populate chartEntries - yAxis
+            //            let dateFormatter = DateFormatter()
+            //            dateFormatter.timeStyle = .medium
+            //            dateFormatter.dateStyle = .none
+            //            xStrings.append("\(dateFormatter.string(from: Date.init(timeIntervalSince1970: TimeInterval(entry.timestamp))))") //populate xAxis
+            xStrings.append(String(price.timestamp))
+        }
+        let set: LineChartDataSet = LineChartDataSet(entries: chartEntries, label: "$")
+//        let mainColor = NSUIColor.init().SettingsService.mainColor
+//        set.setColor(NSUIC)
+        set.setColor(NSUIColor.red, alpha: CGFloat(1))
+        set.circleColors = [NSUIColor.blue]
+        set.circleRadius = 3
+        set.mode = LineChartDataSet.Mode.linear
+        
+        let data: LineChartData = LineChartData(dataSet: set)
+        //        self.graphView.xAxis.labelRotationAngle = -90 //tilts label
+        //        self.graphView.xAxis.valueFormatter = DefaultAxisValueFormatter(block: { (index, axis) -> String in
+        //            print("Index = \(index) --- Axis \(axis)")
+        //            return String(self.timeStamps[Int(index)])
+        //        })
+        self.graphView.xAxis.valueFormatter = DefaultAxisValueFormatter(block: { (index, axis) -> String in
+            //            print("Axis = \(axis?.axisMaxLabels)")
+            return String(self.timeStamps[Int(index)]) //goes out of index if changed
+        })
+        self.graphView.xAxis.axisMaxLabels = timeStamps.count
+        self.graphView.xAxis.setLabelCount(timeStamps.count, force: true)
+        self.graphView.xAxis.labelPosition = .bottom //put xAxis labels at the bottom, default at top
+        self.graphView.data = data
+        
+    }
+}
