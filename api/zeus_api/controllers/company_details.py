487146d611a804dffceb5e0edcb8cb82eb449f95
Andre Williams <awjrs22@gmail.com>
2020-02-24 19:29:57 -0800

    update

--- api/zeus_api/controllers/company_details.py
+++ api/zeus_api/controllers/company_details.py
@@ -13,46 +13,57 @@ from alpha_vantage.timeseries import TimeSeries #api for realtime stock data
 from alpha_vantage.techindicators import TechIndicators  # calculate sma for us
 
 
+api_key = 'IOLIXAXKGPI3A4QM'
+# turns data into a pandas dataframe
+ts = TimeSeries(key=api_key, output_format='pandas')
+# all data, every minute, from MSFT
+data_ts, meta_data_ts = ts.get_intraday(symbol=new_data, interval='1min', outputsize='full')  
+# we only want the closing column
+df2 = data_ts['4. close'].iloc[-1]
+# prints last closing price of chosen company
+#print('this is data: ' + str(df2))
+
+
+# helper function
+def api(self, new_data, token):
+    api_key = 'IOLIXAXKGPI3A4QM'
+    ts = TimeSeries(key=api_key, output_format='pandas') # turns data into a pandas dataframe
+    data_ts, meta_data_ts = ts.get_intraday(symbol=new_data, interval='1min', outputsize='full') # all data, every minute, from MSFT
+    period = 60
+    ti = TechIndicators(key=api_key, output_format='pandas')
+    data_ti, meta_data_ti = ti.get_sma(symbol='MSFT', interval='1min', time_period=period, series_type='close')
+    df2 = data_ts['4. close'].iloc[-1] # we only want the closing column
+    return df2
+# Company and Recent Price
 class companyDetails(Resource):
     @token_required
-    def get(self, data, token):
-        data = str(data)
-        data = data.lower()
-        companies = { 'microsoft': 'MSFT', 'apple': 'AAPL', 'amazon':'AMZN', 'facebook':'FB', 'bank of america': 'BAC', 'boeing':'BA', 'intel','INTC', 'johnson&johnson': 'JNJ'}
-        #companies = {}
+    def get(self):
+        data = request.get_json()
+        company = str(data.get('companyname')).lower()
+        companies = { 'microsoft': 'MSFT', 'apple': 'AAPL', 'amazon':'AMZN', 'facebook':'FB', 'bank of america': 'BAC', 'boeing':'BA', 'intel':'INTC', 'johnson&johnson': 'JNJ'}
         for item in companies:
-            if data in companies:
-                new_data = companies.get(item)
-                return new_data
+            if company in companies:
+                ticker = companies.get(item)
+                closing_price = api(ticker)
             else:
                 return None
+        return jsonify({'company': ticker,'price': closing_price})
 
-        api_key = 'IOLIXAXKGPI3A4QM'
+   
 
-        # turns data into a pandas dataframe
-        ts = TimeSeries(key=api_key, output_format='pandas')
-        data_ts, meta_data_ts = ts.get_intraday(
-            symbol=new_data, interval='1min', outputsize='full')  # all data, every minute, from MSFT
-        # we only want the closing column
-        df2 = data_ts['4. close'].iloc[period-1::]
-        # prints last closing price of chosen company
-        print('this is data: ' + str(df2))
+
+
+    #return ('this is data: ' +  str(new_data) + str( df2)) # prints last closing price of chosen company
 
         # if data is None or token is None:
         #     return jsonify({'message' 'empty data or token variable'})
 
         # company = zeus_api.company.find_one(
         #     {'_id': ObjectId(data.get('uuid'))})
-        # print(data)
-        # print(token)
-        # print(company)
+
 
 # Example 
 # company = companyDetails()
 # company.get(microsoft)
 
-        return jsonify({'success': 'returned company'})
-
-    def post(self):
-        # todo later
-        return jsonify({'message': 'hi'})
+       
