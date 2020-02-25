b9001c00818d74ad1c0282433440eabafb6d441f
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-24 18:45:51 -0800

    [add] more different dummy stocks data and save it

--- mobile/ZeusApp/ZeusApp/Controllers/TrendingVC.swift
+++ mobile/ZeusApp/ZeusApp/Controllers/TrendingVC.swift
@@ -67,15 +67,16 @@ class TrendingVC: UIViewController {
         let stock2 = Stock(_name: "Etherium", _shortName: "ETH", _price: "80", _imageUrl: "", _rank: 2)
         let stock3 = Stock(_name: "Tesla", _shortName: "TSL", _price: "600", _imageUrl: "", _rank: 3)
         let stock4 = Stock(_name: "Apple", _shortName: "APL", _price: "8,900", _imageUrl: "", _rank: 4)
-        let stock5 = Stock(_name: "Bitcoin", _shortName: "BTC", _price: "8,900", _imageUrl: "", _rank: 5)
-        let stock6 = Stock(_name: "Etherium", _shortName: "ETH", _price: "80", _imageUrl: "", _rank: 6)
-        let stock7 = Stock(_name: "Tesla", _shortName: "TSL", _price: "600", _imageUrl: "", _rank: 7)
-        let stock8 = Stock(_name: "Apple", _shortName: "APL", _price: "8,900", _imageUrl: "", _rank: 8)
-        let stock9 = Stock(_name: "Bitcoin", _shortName: "BTC", _price: "8,900", _imageUrl: "", _rank: 9)
-        let stock10 = Stock(_name: "Etherium", _shortName: "ETH", _price: "80", _imageUrl: "", _rank: 10)
-        let stock11 = Stock(_name: "Tesla", _shortName: "TSL", _price: "600", _imageUrl: "", _rank: 11)
-        let stock12 = Stock(_name: "Apple", _shortName: "APL", _price: "8,900", _imageUrl: "", _rank: 12)
+        let stock5 = Stock(_name: "Amazon", _shortName: "AMZ", _price: "1,900", _imageUrl: "", _rank: 5)
+        let stock6 = Stock(_name: "Twitter", _shortName: "TWTR", _price: "35.61", _imageUrl: "", _rank: 6)
+        let stock7 = Stock(_name: "Facebook", _shortName: "FB", _price: "199.60", _imageUrl: "", _rank: 7)
+        let stock8 = Stock(_name: "Microsoft", _shortName: "MSFT", _price: "172.88", _imageUrl: "", _rank: 8)
+        let stock9 = Stock(_name: "Netflix", _shortName: "NFLX", _price: "369.80", _imageUrl: "", _rank: 9)
+        let stock10 = Stock(_name: "Starbucks", _shortName: "SBUX", _price: "84.95", _imageUrl: "", _rank: 10)
+        let stock11 = Stock(_name: "Rite Aid", _shortName: "RAD", _price: "15.16", _imageUrl: "", _rank: 11)
+        let stock12 = Stock(_name: "IBM", _shortName: "IBM", _price: "146.89", _imageUrl: "", _rank: 12)
         stocks.append(contentsOf: [stock1, stock2, stock3, stock4, stock5, stock6, stock7, stock8, stock9, stock10, stock11, stock12])
+        saveTrendingStocks(stocks: self.stocks)
     }
     
 //MARK: IBActions
@@ -113,6 +114,10 @@ extension TrendingVC: UITableViewDataSource {
 //MARK: StockDetailsProtocol
 extension TrendingVC: StockDetailProtocol {
     func didUpdateStock(stock: Stock) {
-        print(stock.name)
+        print("Updated stock =",stock.name)
+        guard let user = getCurrentUser() else { return }
+        for favStock in user.stocks where stock.name == favStock {
+//            user.delete(<#T##stockIndex: Int##Int#>)
+        }
     }
 }
