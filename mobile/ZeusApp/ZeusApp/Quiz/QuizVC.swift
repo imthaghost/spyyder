666c13ac92838e66f1a0fb8cb98a564a3066ed5e
Samuel Folledo <samuelfolledo@gmail.com>
2020-03-01 14:39:51 -0800

    [add] populate views

--- mobile/ZeusApp/ZeusApp/Quiz/QuizVC.swift
+++ mobile/ZeusApp/ZeusApp/Quiz/QuizVC.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class QuizVC: UIViewController {
 //MARK: Properties
+    var pages: [UIView] = []
+    var currentPage: Int = 0
+    var numberOfPages: Int {
+        get {
+            return self.pages.count
+        }
+    }
     
 //MARK: IBOutlets
     @IBOutlet weak var mainStackView: UIStackView!
     @IBOutlet weak var continueButton: UIButton!
-    
+        
 //MARK: App Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +39,26 @@ class QuizVC: UIViewController {
         mainStackView.axis = .horizontal
         
         updateColors()
+        setupPageViews()
+    }
+    
+    fileprivate func setupPageViews() {
+        let page1View = Page1View()
+        let page2View = Page2View()
+        let page3View = Page3View()
+        let page4View = Page4View()
+        let page5View = Page5View()
+        let page6View = Page6View()
+        let page7View = Page7View()
+        pages.append(contentsOf: [page1View, page2View, page3View, page4View, page5View, page6View, page7View])
+        for page in pages {
+            page.translatesAutoresizingMaskIntoConstraints = false
+            mainStackView.addArrangedSubview(page)
+            NSLayoutConstraint.activate(
+                page.pinToEdges(view: mainStackView)
+            )
+            page.widthAnchor.constraint(equalToConstant: self.view.frame.size.width).isActive = true
+        }
     }
     
     fileprivate func setupNavigationBar() {
