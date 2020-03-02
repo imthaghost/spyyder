4ef0c48719c77ddda590b4a9739e2b8f22fe566a
Samuel Folledo <samuelfolledo@gmail.com>
2020-03-01 16:19:05 -0800

    [add] extension to pin to edges

--- mobile/ZeusApp/ZeusApp/Helpers/Extensions/UIView+extensions.swift
+++ mobile/ZeusApp/ZeusApp/Helpers/Extensions/UIView+extensions.swift
@@ -1,15 +1,15 @@
 //
-//  UILayoutGuide+extensions.swift
+//  UIView+extensions.swift
 //  ZeusApp
 //
-//  Created by Macbook Pro 15 on 2/3/20.
+//  Created by Macbook Pro 15 on 3/1/20.
 //  Copyright © 2020 SamuelFolledo. All rights reserved.
 //
 
 import UIKit
 
-extension UILayoutGuide {
-    func pinToEdgesEqually(view: UIView, inset: UIEdgeInsets = UIEdgeInsets.zero) -> [NSLayoutConstraint] { //pins view to edge - returns array of constraints that needs to be activated
+extension UIView {
+    func pinToEdges(view: UIView, inset: UIEdgeInsets = UIEdgeInsets.zero) -> [NSLayoutConstraint] { //pins view to edge - returns array of constraints that needs to be activated
         return [
             leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: inset.left),
             trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: inset.right),
