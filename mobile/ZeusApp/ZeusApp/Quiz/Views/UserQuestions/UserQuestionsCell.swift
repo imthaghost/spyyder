b07e352342a973361887ebca46a9a6ce35df1276
Samuel Folledo <samuelfolledo@gmail.com>
2020-03-02 23:06:20 -0800

    [add] cell for user questions

---
+++ mobile/ZeusApp/ZeusApp/Quiz/Views/UserQuestions/UserQuestionsCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserQuestionsCell.swift
+//  ZeusApp
+//
+//  Created by Macbook Pro 15 on 3/2/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class UserQuestionsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
